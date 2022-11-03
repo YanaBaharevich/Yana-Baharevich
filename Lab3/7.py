9fa36135416cf46c7e2fa993bd10dd466b3a0f1a
YanaBaharevich <w68369@student.wsiz.edu.pl>
2022-11-03 18:14:17 +0100

    Lab3

---
+++ Lab3/7.py
@@ -0,0 +1,14 @@
+for i in range(7,84,7):
+ print(i, end=" ")
+print()
+
+for i in range(1,101):
+ print(i, end=" ")
+print()
+
+for i in range(100,-1,-1):
+ print(i, end=" ")
+print()
+
+for i in range(20,0,2):
+ print(i, end=" ")
