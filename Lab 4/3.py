053f9aebc30a81f6d0540527238e59067d93ade3
w68369 <w68369@student.wsiz.edu.pl>
2022-11-10 18:47:59 +0100

    Lab 4

---
+++ Lab 4/3.py
@@ -0,0 +1,7 @@
+zwierzeta=[]
+for i in range(4):
+    a=str(input("dodaj zwierzetów:"))
+    zwierzeta.append(a)
+print(zwierzeta)
+zwierzeta.sort()
+print(zwierzeta[0],zwierzeta[-3:],len(zwierzeta) )
---
+++ Lab 4/4.py
@@ -0,0 +1,27 @@
+import random
+punkty = []
+x=0
+for i in range (15):
+    a = random.uniform(0, 50)
+    a=round(a,2)
+    x=round(x, 2)
+    punkty.append(a)
+    x=x+a
+print(punkty)
+b=float(input("Podaj punkty:"))
+if a in punkty:
+    print(punkty.index(b))
+else:
+    print("Nie ma takich punktów")
+print("Maximalna liczba:",max(punkty))
+print("Minimalna liczba:",min(punkty))
+print("Srednia liczba punktów:", round(x/15,2))
+punkty_1=[]
+punkty_2=[]
+for i in range(15):
+ if punkty[i]>x/15:
+     punkty_1.append(punkty[i])
+ elif punkty[i]<x/15:
+     punkty_2.append(punkty[i])
+print("powyżej średniej:",punkty_1)
+print("poniżej średniej:",punkty_2)
